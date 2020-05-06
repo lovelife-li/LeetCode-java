ec208cee3e91201b7019a037f14bafcf3b67db74
liumin <liumin@cmos.chinamobile.com>
2020-05-07 00:00:10 +0800

    add some question

--- src/main/java/com/leetcode/medium/Solution_002.java
+++ src/main/java/com/leetcode/medium/Solution_002.java
@@ -9,9 +9,8 @@ import java.util.HashMap;
 /**
  * @author ldb
  * @date 2020-03-27 9:46
- * @description <p>
- * 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储一位数字。
- * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+ * @description <p> 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，
+ * 并且它们的每个节点只能存储一位数字。 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
  * 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
  * <p>
  * Example:
@@ -22,17 +21,29 @@ import java.util.HashMap;
 public class Solution_002 {
 
     public static ListNode addTwoNumbers1(ListNode list1, ListNode list2) {
+
+        ListNode n1 = list1, n2 = list2;
+        ListNode resNode = new ListNode(-1);
+        ListNode cur = resNode;
         int bit = 0;
-        if (list1 == null && list2 == null) {
-            return null;
+        while (n1 != null || n2 != null) {
+            int a = n1 != null ? n1.val : 0;
+            int b = n2 != null ? n2.val : 0;
+            cur.next = new ListNode((a + b + bit) % 10);
+            bit = (a + b + bit) / 10;
+            cur = cur.next;
+            if (n1 != null) {
+                n1 = n1.next;
+            }
+            if (n2 != null) {
+                n2 = n2.next;
+            }
         }
-        if (list1 == null) {
+        if (bit>0){
+            cur.next = new ListNode(1);
+        }
 
-        }
-        while (list1 != null || list2 != null) {
-
-        }
-        return null;
+        return resNode.next;
 
     }
 
@@ -43,7 +54,9 @@ public class Solution_002 {
     }
 
     public static void main(String[] args) {
-
-
+        ListNode list1 = ListNode.createList(new int[]{9, 9, 9});
+        ListNode list2 = ListNode.createList(new int[]{9, 9, 9});
+        ListNode res = addTwoNumbers1(list1, list2);
+        ListNode.print(res);
     }
 }
--- src/main/java/com/leetcode/medium/Solution_003.java
+++ src/main/java/com/leetcode/medium/Solution_003.java
@@ -1,17 +1,16 @@
 package com.leetcode.medium;
 
 
-import java.util.Arrays;
-import java.util.HashMap;
+import static java.lang.Math.max;
 
 /**
  * @author ldb
  * @date 2020-03-27 9:46
  * @description <p>
- *     给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+ * 给定一个字符串，请你找出其中不含有重复字符的 最长子串的长度。
  * <p>
  * Example:
- * 输入: "abcabcbb"
+ * 输入: "abcabcbb"   "abcaaabcbb"
  * 输出: 3
  * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
  */
@@ -21,43 +20,29 @@ public class Solution_003 {
     /**
      * 两层循环遍历所有元素
      */
-    public static int[] twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length - 1; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                int res = nums[i] + nums[j];
-                if (res == target) {
-                    return new int[]{
-                            i, j
-                    };
-                }
+    public static int lengthOfLongestSubstring(String s) {
+        int[] freq = new int[256];
+        int l = 0, r = -1; //滑动窗口为s[l...r]
+        int res = 0;
+        // 整个循环从 l == 0; r == -1 这个空窗口开始
+        // 到l == s.length(); r == s.length()-1 这个空窗口截止
+        // 在每次循环里逐渐改变窗口, 维护freq, 并记录当前窗口中是否找到了一个新的最优值
+        while (l < s.length()) {
+            if (r + 1 < s.length() && freq[s.charAt(r + 1)] == 0) {
+                r++;
+                freq[s.charAt(r)]++;
+            } else {   //r已经到头 || freq[s.charA(t+1)] == 1
+                freq[s.charAt(l)]--;
+                l++;
             }
+            res = max(res, r - l + 1);
         }
-        return null;
+        return res;
 
     }
 
-    /**
-     * 利用 HashMap 作为存储，键为目标值减去当前元素值，索引为值，比如 `i = 0` 时，此时首先要判断 `nums[0] = 2` 是否在 map 中，如果不存在，
-     * 那么插入键值对 `key = 9 - 2 = 7, value = 0`，之后当 `i = 1` 时，此时判断 `nums[1] = 7` 已存在于 map 中，那么取出该 `value = 0`
-     * 作为第一个返回值，当前 `i` 作为第二个返回值，具体代码如下所示。
-     */
-    public static int[] twoSum2(int[] nums, int target) {
-        HashMap<Integer, Integer> map = new HashMap<>(16);
-        for (int i = 0; i < nums.length; i++) {
-            if (map.get(nums[i]) != null) {
-                return new int[]{map.get(nums[i]),i};
-            }
-            map.put(target - nums[i],i);
-
-        }
-        return null;
-
-    }
 
     public static void main(String[] args) {
-        int[] nums = {2, 11, 15, 2, 7};
-        int[] res = twoSum2(nums, 9);
-        System.out.println(Arrays.toString(res));
 
 
     }
